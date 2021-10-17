c5712310c3bae11ad98abb36096cf1a6ea835da5
Tony ID <tony.inuma@gmail.com>
2021-10-17 11:18:22 -0500

    Add: CounterApp component

---
+++ src/components/CounterApp.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const CounterApp = () => {
+    return (
+        <>
+            <h1>Counter {0}</h1>
+            <hr />
+            <button className="btn btn-primary">+1</button>
+        </>
+    );
+};
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { CounterApp } from "./components/CounterApp";
 import { HookApp } from "./HookApp";
 
-ReactDOM.render(<HookApp />, document.getElementById("root"));
+ReactDOM.render(<CounterApp />, document.getElementById("root"));
