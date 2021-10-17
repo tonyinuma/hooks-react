740d49bf8902b203f2d8c43960feb95e2d83c8b0
Tony ID <tony.inuma@gmail.com>
2021-10-17 18:33:02 -0500

    Add: Simple Form component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 //import { CounterApp } from "./components/CounterApp";
-import { CounterWithCustomHook } from "./components/Counter/CounterWithCustomHook";
+//import { CounterWithCustomHook } from "./components/Counter/CounterWithCustomHook";
+import { SimpleForm } from "./components/Form/SimpleForm";
 //import { HookApp } from "./HookApp";
 
-ReactDOM.render(<CounterWithCustomHook />, document.getElementById("root"));
+ReactDOM.render(<SimpleForm />, document.getElementById("root"));
