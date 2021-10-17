11d736b01168d7d791cecdb121f4f5387e960846
Tony ID <tony.inuma@gmail.com>
2021-10-17 16:19:43 -0500

    Add: Counter with Custom Hook

---
+++ src/components/CounterWithCustomHook.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { useCounter } from "../hooks/useCounter";
+import "./counter.css";
+
+export const CounterWithCustomHook = () => {
+    const { state, increment, decrement, reset } = useCounter(100);
+
+    return (
+        <>
+            <h1>Counter {state}</h1>
+            <hr />
+            <button className="btn btn-primary" onClick={() => increment(2)}>
+                +1
+            </button>
+            <button className="btn btn-primary" onClick={() => reset()}>
+                Reset
+            </button>
+            <button className="btn btn-primary" onClick={() => decrement(2)}>
+                -1
+            </button>
+        </>
+    );
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { CounterApp } from "./components/CounterApp";
+//import { CounterApp } from "./components/CounterApp";
+import { CounterWithCustomHook } from "./components/CounterWithCustomHook";
 //import { HookApp } from "./HookApp";
 
-ReactDOM.render(<CounterApp />, document.getElementById("root"));
+ReactDOM.render(<CounterWithCustomHook />, document.getElementById("root"));
