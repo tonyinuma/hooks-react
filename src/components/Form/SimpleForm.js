6b593268c5047be705c3d408e79452c6c695f54b
Tony ID <tony.inuma@gmail.com>
2021-10-18 19:00:25 -0500

    Add: Use Effect simple form

--- src/components/Form/SimpleForm.js
+++ src/components/Form/SimpleForm.js
@@ -1,11 +1,60 @@
-import React from "react";
-import './effects.css'
+import React, { useEffect, useState } from "react";
+import "./effects.css";
 
 export const SimpleForm = () => {
+    const [formState, setFormState] = useState({
+        name: "",
+        email: "",
+    });
+
+    const { name, email } = formState;
+
+    useEffect(() => {
+        console.log("Heey");
+    }, []);
+
+    useEffect(() => {
+        console.log("formState");
+    }, [formState]);
+
+    useEffect(() => {
+        console.log("Email Cambió");
+    }, [email]);
+
+    const hanleInputChange = ({ target }) => {
+        setFormState({
+            ...formState,
+            [target.name]: target.value,
+        });
+    };
+
     return (
         <>
             <h1>Use Effect</h1>
             <hr />
+            <div className="form-group">
+                <input
+                    type="text"
+                    name="name"
+                    className="form-control"
+                    placeholder="Tu nombre"
+                    autoComplete="off"
+                    value={name}
+                    onChange={hanleInputChange}
+                />
+            </div>
+
+            <div className="form-group mt-2">
+                <input
+                    type="text"
+                    name="email"
+                    className="form-control"
+                    placeholder="Tu Email"
+                    autoComplete="off"
+                    value={email}
+                    onChange={hanleInputChange}
+                />
+            </div>
         </>
     );
 };
