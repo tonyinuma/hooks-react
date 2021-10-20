4e082de5317e12c217496da4baacc0a2e782dff3
Tony ID <tony.inuma@gmail.com>
2021-10-19 20:44:34 -0500

    Add: Clean up in useEffect

--- src/components/Form/Message.js
+++ src/components/Form/Message.js
@@ -1,17 +1,28 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from "react";
 
 export const Message = () => {
+    const [coords, setCoords] = useState({ x: 0, y: 0 });
+    const { x, y } = coords;
 
     useEffect(() => {
-        console.log('Componente montado');
+        const mouseMove = (e) => {
+            const coords = { x: e.x, y: e.y };
+            setCoords(coords);
+        };
+        
+        window.addEventListener("mousemove", mouseMove);
+
         return () => {
-            console.log('Componente desmontado');
-        }
-    }, [])
+            window.removeEventListener("mousemove", mouseMove);
+        };
+    }, []);
 
     return (
         <div>
             <h3>You are great</h3>
+            <p>
+                x:{x} -- y:{y}
+            </p>
         </div>
-    )
-}
+    );
+};
