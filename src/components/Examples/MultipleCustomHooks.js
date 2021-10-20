ba8860ae2c41eb738e0ec2fb7490414cd8a01d0b
Tony ID <tony.inuma@gmail.com>
2021-10-19 23:38:53 -0500

    Add: View completed using custom hooks, useCounter and useFetch

--- src/components/Examples/MultipleCustomHooks.js
+++ src/components/Examples/MultipleCustomHooks.js
@@ -1,15 +1,30 @@
-import React from 'react'
-import { useFetch } from '../../hooks/useFetch';
-import '../Form/effects.css';
+import React from "react";
+import { useCounter } from "../../hooks/useCounter";
+import { useFetch } from "../../hooks/useFetch";
+import "../Form/effects.css";
 
 export const MultipleCustomHooks = () => {
-
-    const state = useFetch(`https://www.breakingbadapi.com/api/quotes/1`);
-    console.log(state);
+    const { state, increment } = useCounter(1);
+    const { loading, data } = useFetch(
+        `https://www.breakingbadapi.com/api/quotes/${state}`
+    );
+    const { author, quote } = !!data && data[0];
 
     return (
         <div>
-            <h1>Hola de nuevo</h1>
+            <h1>Breaking Bad</h1>
+            <hr />
+            {loading ? (
+                <div className="alert alert-info text-center">Loading...</div>
+            ) : (
+                <blockquote className="blockquote text-end">
+                    <p>{quote}</p>
+                    <footer className="blockquote-footer">{author}</footer>
+                </blockquote>
+            )}
+            <button className="btn btn-primary" onClick={increment}>
+                Siguiente Frase
+            </button>
         </div>
-    )
-}
+    );
+};
