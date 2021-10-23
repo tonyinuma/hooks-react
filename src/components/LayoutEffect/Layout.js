c5b991bfb9541faabfc6c051f3f17ac06ae23582
Tony ID <tony.inuma@gmail.com>
2021-10-22 22:41:31 -0500

    Add: useLayoutEffect example, get width sizes of layouts

--- src/components/LayoutEffect/Layout.js
+++ src/components/LayoutEffect/Layout.js
@@ -1,22 +1,31 @@
-import React from "react";
+import React, { useLayoutEffect, useRef, useState } from "react";
 import { useCounter } from "../../hooks/useCounter";
 import { useFetch } from "../../hooks/useFetch";
-import "../Form/effects.css";
+import "./layout.css";
 
 export const Layout = () => {
     const { state, increment } = useCounter(1);
+    const [boxSize, setBoxSize] = useState({})
     const { data } = useFetch(
         `https://www.breakingbadapi.com/api/quotes/${state}`
     );
     const { quote } = !!data && data[0];
+
+    const pTag = useRef();
+
+    const res = useLayoutEffect(() => {
+        const data = pTag.current.getBoundingClientRect();
+        setBoxSize(data)
+    }, [quote]);
 
     return (
         <div>
             <h1>Layout Effect</h1>
             <hr />
             <blockquote className="blockquote text-end">
-                <p>{quote}</p>
+                <p ref={pTag}>{quote}</p>
             </blockquote>
+            <pre>{JSON.stringify(boxSize, null, 3)}</pre>
             <button className="btn btn-primary" onClick={increment}>
                 Siguiente Frase
             </button>
