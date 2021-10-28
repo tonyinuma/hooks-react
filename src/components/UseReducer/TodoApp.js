4703656924a0f8f43de265560598e58a039ce966
Tony ID <tony.inuma@gmail.com>
2021-10-28 17:48:33 -0500

    Add: List Todos Design

--- src/components/UseReducer/TodoApp.js
+++ src/components/UseReducer/TodoApp.js
@@ -15,14 +15,39 @@ export const TodoApp = () => {
     console.log(todos);
     return (
         <>
-            <h1>Todo App</h1>
+            <h1>Todo App ({todos.length})</h1>
             <hr />
 
-            <ul>
-                <li>Hi</li>
-                <li>Dark</li>
-                <li>World!</li>
-            </ul>
+            <div className="row">
+                <div className="col-7">
+                    <ul className="list-group list-group-flush">
+                        {todos.map((todo, index) => (
+                            <li key={todo.id} className="list-group-item">
+                                <p className="text-center">
+                                    {index + 1}. {todo.desc}
+                                </p>
+                                <button className="btn btn-danger">
+                                    Remove
+                                </button>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <div className="col-5">
+                    <h4>Agregar Todo</h4>
+                    <hr />
+                    <form>
+                        <input
+                            type="text"
+                            name="description"
+                            placeholder="write todo .."
+                            autoComplete="off"
+                            className="form-control"
+                        />
+                        <button type="button" className="btn btn-dark mt-2 w-100">Add</button>
+                    </form>
+                </div>
+            </div>
         </>
     );
 };
