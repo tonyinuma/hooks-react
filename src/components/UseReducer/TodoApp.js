aa48bb8e812c955f3574225a6b33f6fdc0a13dc5
Tony ID <tony.inuma@gmail.com>
2021-10-28 17:20:31 -0500

    Add: Todos list using useReducer

--- src/components/UseReducer/TodoApp.js
+++ src/components/UseReducer/TodoApp.js
@@ -1,7 +1,18 @@
 import React, { useReducer } from "react";
+import { todoReducer } from "./todoReducer";
 import "./style.css";
 
+const initialState = [
+    {
+        id: new Date().getTime(),
+        desc: "Aprender React",
+        done: false,
+    },
+];
+
 export const TodoApp = () => {
+    const [todos] = useReducer(todoReducer, initialState);
+    console.log(todos);
     return (
         <>
             <h1>Todo App</h1>
---
+++ src/components/UseReducer/todoReducer.js
@@ -0,0 +1,9 @@
+export const todoReducer = (state = [], action) => {
+    switch (action.type) {
+        // case "Add":
+            // break;
+
+        default:
+            return state;
+    }
+};
