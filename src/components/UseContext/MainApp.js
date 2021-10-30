1cfce0879ba0a9ee7a2f07391f18b743ed754e9d
Tony ID <tony.inuma@gmail.com>
2021-10-29 23:14:58 -0500

    Add: AppRouter to MainApp and this to Index to watch

--- src/components/UseContext/MainApp.js
+++ src/components/UseContext/MainApp.js
@@ -1,10 +1,6 @@
 import React from "react";
+import { AppRouter } from "./AppRouter";
 
 export const MainApp = () => {
-    return (
-        <>
-            <h1>MainApp</h1>
-            <hr />
-        </>
-    );
+    return <AppRouter />;
 };
