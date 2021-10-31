4164ac9f38d92556d57983f92b538cf73c22c3ef
Tony ID <tony.inuma@gmail.com>
2021-10-31 00:10:58 -0500

    Add: Design to nav and NavLink Component using active

--- src/components/UseContext/NavBar.js
+++ src/components/UseContext/NavBar.js
@@ -1,20 +1,40 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 export const NavBar = () => {
     return (
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/about">About</Link>
-                </li>
-                <li>
-                    <Link to="/login">Login</Link>
-                </li>
-            </ul>
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+            <Link to="/" className="navbar-brand">
+                Context
+            </Link>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="navbar-nav">
+                    <NavLink
+                        exact
+                        activeClassName="active"
+                        to="/"
+                        className="nav-item nav-link"
+                    >
+                        Home
+                    </NavLink>
+                    <NavLink
+                        exact
+                        activeClassName="active"
+                        to="/about"
+                        className="nav-item nav-link"
+                    >
+                        About
+                    </NavLink>
+                    <NavLink
+                        exact
+                        activeClassName="active"
+                        to="/login"
+                        className="nav-item nav-link"
+                    >
+                        Login
+                    </NavLink>
+                </div>
+            </div>
         </nav>
-    )
-}
+    );
+};
