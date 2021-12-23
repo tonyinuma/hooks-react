c48e32e334b2869c27ab3f9662d1a6daaeaa3b0b
Tony ID <tony.inuma@gmail.com>
2021-12-23 18:41:07 -0500

    Feat: Context userContext

--- src/components/UseContext/HomeScreen.js
+++ src/components/UseContext/HomeScreen.js
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
+import { UserContext } from "./UserContext";
 
 export const HomeScreen = () => {
+
+    const userContext = useContext(UserContext);
+
     return (
         <>
             <h1>HomeScreen</h1>
--- src/components/UseContext/MainApp.js
+++ src/components/UseContext/MainApp.js
@@ -1,6 +1,17 @@
 import React from "react";
 import { AppRouter } from "./AppRouter";
+import { UserContext } from "./UserContext";
 
 export const MainApp = () => {
-    return <AppRouter />;
+    const user = {
+        id: 1234,
+        name: "tony",
+        email: "tony.inuma@gmail.com",
+    };
+
+    return (
+        <UserContext.Provider value={user}>
+            <AppRouter />
+        </UserContext.Provider>
+    );
 };
